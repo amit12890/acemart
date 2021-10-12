a29bf928ebaf1e7fe40318b22654443f6e3e82e8
Dishant Chavda <dishant.chadva@magedelight.com>
2021-10-12 14:56:14 +0530

    update/test loginPage with same signIn component

--- src/components/LoginPage/loginPage.js
+++ src/components/LoginPage/loginPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { useStyle } from '../../venia/classify';
+import SignIn from '../../venia/components/SignIn';
 import defaultClasses from './loginPage.css';
 
 const LoginPage = (props) => {
     const classes = useStyle(defaultClasses, props.classes);
 
     return (
-        <div className={classes.wrapper}>This will be new login page, with hot reload !!</div>
+        <div className={classes.wrapper}><SignIn /></div>
     );
 }
 
