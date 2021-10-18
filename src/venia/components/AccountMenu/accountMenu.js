af62797c566be604edeab34bfd47e66aa7d6b32f
Dishant Chavda <dishant.chadva@magedelight.com>
2021-10-18 14:39:11 +0530

    update/added mask for overlay

--- src/venia/components/AccountMenu/accountMenu.js
+++ src/venia/components/AccountMenu/accountMenu.js
@@ -5,6 +5,7 @@ import { useAccountMenu } from '@magento/peregrine/lib/talons/Header/useAccountM
 
 import { useStyle } from '../../classify';
 import { Portal } from '@magento/venia-ui/lib/components/Portal';
+import Mask from '@magento/venia-ui/lib/components/Mask';
 import CreateAccount from '../CreateAccount';
 import SignIn from '../SignIn';
 import AccountMenuItems from './accountMenuItems';
@@ -90,6 +91,7 @@ const AccountMenu = React.forwardRef((props, ref) => {
 
     return (
         <Portal>
+            <Mask isActive={accountMenuIsOpen} />
             <div className={rootClass}>
                 <div ref={ref} className={contentsClass}>
                     {accountMenuIsOpen ? dropdownContents : null}
