c16f8613f94f3668923043160620d7e5c68352c1
Vihas <vihas.shah@krishtechnolabs.com>
2021-11-10 13:01:07 +0530

    [ACEMART-75] PWA: Landing Page - search by default open issue resolved

--- src/venia/components/Header/header.js
+++ src/venia/components/Header/header.js
@@ -36,7 +36,7 @@ const Header = props => {
     ) : null;
 
     return (
-        <Fragment>  
+        <Fragment>
             <header className={rootClass}>
                 {/** This is top NoticeBar */}
                 <div className={[classes.panelWrapper, classes.headerNotice].join(" ")}>
@@ -46,7 +46,7 @@ const Header = props => {
                                 to={resourceUrl('/careers')}
                                 className={classes.action}
                             >
-                            <span>NOW HIRING – CLICK HERE TO JOIN OUR TEAM!</span>
+                                <span>NOW HIRING – CLICK HERE TO JOIN OUR TEAM!</span>
                             </Link>
                         </p>
                     </div>
@@ -61,14 +61,14 @@ const Header = props => {
                                         to={resourceUrl('/supply-chain')}
                                         className={classes.action}
                                     >
-                                        <i className={classes.iconWrapper}> 
+                                        <i className={classes.iconWrapper}>
                                             <svg className={classes.svgIcon} version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32">
                                                 <title>back</title>
                                                 <path d="M32 20.281q0 1.875-0.75 4.109t-1.531 3.953q-0.125 0.313-0.281 0.688t-0.375 0.656q-0.094 0.156-0.203 0.234t-0.297 0.078q-0.25 0-0.406-0.188t-0.156-0.438q0-0.219 0.031-0.453t0.063-0.453q0.031-0.531 0.063-1.078t0.031-1.109q0-3.188-0.906-5.156-0.938-1.969-2.594-3.047t-4.031-1.422q-2.344-0.375-5.219-0.375h-4v4.563q0 0.469-0.344 0.813t-0.813 0.344q-0.219 0-0.422-0.094t-0.391-0.25l-9.125-9.125q-0.156-0.188-0.25-0.391t-0.094-0.422 0.094-0.438 0.25-0.375l9.125-9.125q0.188-0.188 0.391-0.266t0.422-0.078q0.469 0 0.813 0.328t0.344 0.797v4.594h4q2.188 0 4.563 0.25 2.344 0.25 4.5 1.016t3.875 2.203q1.75 1.406 2.688 3.719 0.563 1.406 0.75 2.922t0.188 3.016v0z"></path>
                                             </svg>
-                                        </i> 
+                                        </i>
                                         <span>Supply Chain Issues</span>
-                                    </Link> 
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
@@ -85,9 +85,9 @@ const Header = props => {
                                                 <title>checkmark</title>
                                                 <path d="M27 4l-15 15-7-7-5 5 12 12 20-20z"></path>
                                             </svg>
-                                        </i> 
+                                        </i>
                                         <span>Ace Mart Advantage</span>
-                                    </Link> 
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -99,9 +99,9 @@ const Header = props => {
                                             <svg className={[classes.svgIcon, classes.store].join(" ")} version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32">
                                                 <title>store</title>
                                                 <path d="M32 30v-2h-2v-12h2v-2h-6v2h2v12h-6v-12h2v-2h-6v2h2v12h-6v-12h2v-2h-6v2h2v12h-6v-12h2v-2h-6v2h2v12h-2v2h-2v2h34v-2h-2zM16 0h2l16 10v2h-34v-2z"></path>                                            </svg>
-                                        </i> 
+                                        </i>
                                         <span>Financing</span>
-                                    </Link> 
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -114,9 +114,9 @@ const Header = props => {
                                                 <title>phone-01</title>
                                                 <path d="M25.156 22.438q0 0.469-0.188 1.25t-0.375 1.219q-0.281 0.656-0.938 1.109t-1.25 0.797q-0.781 0.406-1.594 0.656t-1.719 0.25q-1.25 0-2.391-0.406t-2.297-0.844q-0.813-0.281-1.594-0.641t-1.531-0.828q-1.125-0.719-2.344-1.719t-2.375-2.141-2.156-2.359-1.688-2.344q-0.469-0.75-0.828-1.531t-0.672-1.594q-0.406-1.156-0.813-2.297t-0.406-2.391q0-0.906 0.234-1.719t0.672-1.594q0.344-0.625 0.797-1.266t1.109-0.922q0.438-0.188 1.219-0.375t1.25-0.188q0.094 0 0.188 0.016t0.188 0.047q0.281 0.094 0.547 0.594t0.391 0.75q0.438 0.781 0.859 1.547t0.859 1.516q0.219 0.344 0.531 0.766t0.313 0.828q0 0.813-1.625 1.859t-1.625 1.797q0 0.344 0.25 0.766t0.438 0.766q1.375 2.438 3.125 4.188t4.188 3.094q0.313 0.188 0.75 0.453t0.781 0.266q0.719 0 1.781-1.625t1.875-1.625q0.406 0 0.828 0.313t0.766 0.531q0.75 0.438 1.516 0.859t1.516 0.828q0.281 0.156 0.781 0.422t0.594 0.547q0.031 0.094 0.047 0.188t0.016 0.188v0z"></path>
                                             </svg>
-                                        </i> 
+                                        </i>
                                         <span>Contact</span>
-                                    </Link> 
+                                    </Link>
                                 </li>
 
                                 <li>
@@ -129,10 +129,10 @@ const Header = props => {
                                                 <title>career</title>
                                                 <path d="M30 8h-8v-2q0-0.813-0.594-1.406t-1.406-0.594h-8q-0.813 0-1.406 0.594t-0.594 1.406v2h-8q-0.813 0-1.406 0.594t-0.594 1.406v18q0 0.813 0.594 1.406t1.406 0.594h28q0.813 0 1.406-0.594t0.594-1.406v-18q0-0.813-0.594-1.406t-1.406-0.594zM12 6v0 0h8v2h-8v-2zM30 16h-4v3q0 0.406-0.297 0.703t-0.703 0.297h-2q-0.406 0-0.703-0.297t-0.297-0.703v-3h-12v3q0 0.406-0.297 0.703t-0.703 0.297h-2q-0.406 0-0.703-0.297t-0.297-0.703v-3h-4v-2h28v2z"></path>
                                             </svg>
-                                        </i> 
+                                        </i>
                                         <span>Careers</span>
-                                    </Link> 
-                                </li>   
+                                    </Link>
+                                </li>
 
                                 <li>
                                     <Link
@@ -144,10 +144,10 @@ const Header = props => {
                                                 <title>resources</title>
                                                 <path d="M7 4h-6q-0.406 0-0.703 0.297t-0.297 0.703v22q0 0.406 0.297 0.703t0.703 0.297h6q0.406 0 0.703-0.297t0.297-0.703v-22q0-0.406-0.297-0.703t-0.703-0.297zM6 10h-4v-2h4v2zM17 4h-6q-0.406 0-0.703 0.297t-0.297 0.703v22q0 0.406 0.297 0.703t0.703 0.297h6q0.406 0 0.703-0.297t0.297-0.703v-22q0-0.406-0.297-0.703t-0.703-0.297zM16 10h-4v-2h4v2zM23.906 5.531l-5.344 2.719q-0.375 0.188-0.5 0.578t0.031 0.766l9 17.844q0.188 0.375 0.594 0.516t0.75-0.047l5.375-2.719q0.375-0.188 0.5-0.578t-0.063-0.766l-9-17.844q-0.188-0.375-0.578-0.5t-0.766 0.031v0z"></path>
                                             </svg>
-                                        </i> 
+                                        </i>
                                         <span>Resources</span>
-                                    </Link> 
-                                </li>                                                              
+                                    </Link>
+                                </li>
 
                             </ul>
                         </div>
@@ -166,14 +166,14 @@ const Header = props => {
                                 <Logo classes={{ logo: classes.logo }} />
                             </Link>
                         </div>
-                        
+
                         <div className={classes.csContainer}>
                             <i className={classes.iconWrapper}>
                                 <svg className={classes.svgIcon} version="1.1" xmlns="http://www.w3.org/2000/svg" width="37" height="37" viewBox="0 0 37 37">
                                     <title>phone</title>
                                     <path d="M22 20q-1.5 1.5-2 2.75t-2 1.25-3-1.25-3-2.75-2.75-3-1.25-3 1.25-2 2.75-2q0.75-0.75 0.188-2.094t-1.688-2.656-2.438-2.281-2.063-0.969-1.781 0.938-1.969 2.063-1.594 2.063l-0.656 0.938q0 1.5 0.688 3.625 0.719 2.156 1.859 4.422t2.547 4.359q1.438 2.125 2.906 3.594t3.594 2.906q2.094 1.406 4.359 2.547t4.422 1.859q2.125 0.688 3.625 0.688l0.938-0.656t2.063-1.594 2.063-1.969 0.938-1.781-0.969-2.063-2.281-2.438-2.656-1.688-2.094 0.188z"></path>
-                                    </svg>
-                            </i> 
+                                </svg>
+                            </i>
                             <div className={classes.csContent}>
                                 <span className={classes.csLabel}>Customer Service</span>
                                 <Link
@@ -185,7 +185,7 @@ const Header = props => {
                             </div>
                         </div>
                         <div className={classes.searchBarWrapper}>
-                            <SearchBar isOpen={true} ref={searchRef} />
+                            <SearchBar isOpen={isSearchOpen} ref={searchRef} />
                         </div>
                         <div className={classes.storeLocationWrapper}>
                             <div className={classes.switchersContainer}>
@@ -206,7 +206,7 @@ const Header = props => {
                                 isOnline={isOnline}
                             />
 
-                            
+
                             <div className={classes.secondaryActions}>
                                 <AccountTrigger />
                                 <CartTrigger />
@@ -214,7 +214,7 @@ const Header = props => {
                         </div>
                     </div>
                 </div>
-                
+
             </header>
             <div className={classes.navContainer}>
                 <MegaMenu />
