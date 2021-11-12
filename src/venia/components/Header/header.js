85e244fedf4fb231e2838a1bf717478af2836ce6
satishr <satish@magedelight.com>
2021-11-12 19:00:41 +0530

    header icon added

--- src/venia/components/Header/header.js
+++ src/venia/components/Header/header.js
@@ -9,6 +9,8 @@ import NavTrigger from './navTrigger';
 import OnlineIndicator from './onlineIndicator';
 import { useHeader } from '@magento/peregrine/lib/talons/Header/useHeader';
 import resourceUrl from '@magento/peregrine/lib/util/makeUrl';
+import Image from '../Image';
+
 
 import { useStyle } from '../../classify';
 import defaultClasses from './header.css';
@@ -16,6 +18,8 @@ import PageLoadingIndicator from '@magento/venia-ui/lib/components/PageLoadingIn
 import SearchBar from '../SearchBar';
 import StoreSwitcher from './storeSwitcher';
 import CurrencySwitcher from './currencySwitcher';
+import baloonIcon from '../../../assets/balloons.png';
+import truckIcon from '../../../assets/truckicon.png';
 import MegaMenu from '../MegaMenu';
 
 const Header = props => {
@@ -46,7 +50,14 @@ const Header = props => {
                                 to={resourceUrl('/careers')}
                                 className={classes.action}
                             >
-                                <span>NOW HIRING – CLICK HERE TO JOIN OUR TEAM!</span>
+                                <span className={classes.baloonIcon}>
+                                    <Image
+                                        width="20"
+                                        height="22"
+                                        src={baloonIcon}
+
+                                    />
+                                </span> <span>NOW HIRING – CLICK HERE TO JOIN OUR TEAM!</span>
                             </Link>
                         </p>
                     </div>
@@ -61,12 +72,15 @@ const Header = props => {
                                         to={resourceUrl('/supply-chain')}
                                         className={classes.action}
                                     >
-                                        <i className={classes.iconWrapper}>
-                                            <svg className={classes.svgIcon} version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32">
-                                                <title>back</title>
-                                                <path d="M32 20.281q0 1.875-0.75 4.109t-1.531 3.953q-0.125 0.313-0.281 0.688t-0.375 0.656q-0.094 0.156-0.203 0.234t-0.297 0.078q-0.25 0-0.406-0.188t-0.156-0.438q0-0.219 0.031-0.453t0.063-0.453q0.031-0.531 0.063-1.078t0.031-1.109q0-3.188-0.906-5.156-0.938-1.969-2.594-3.047t-4.031-1.422q-2.344-0.375-5.219-0.375h-4v4.563q0 0.469-0.344 0.813t-0.813 0.344q-0.219 0-0.422-0.094t-0.391-0.25l-9.125-9.125q-0.156-0.188-0.25-0.391t-0.094-0.422 0.094-0.438 0.25-0.375l9.125-9.125q0.188-0.188 0.391-0.266t0.422-0.078q0.469 0 0.813 0.328t0.344 0.797v4.594h4q2.188 0 4.563 0.25 2.344 0.25 4.5 1.016t3.875 2.203q1.75 1.406 2.688 3.719 0.563 1.406 0.75 2.922t0.188 3.016v0z"></path>
-                                            </svg>
-                                        </i>
+
+                                        <span className={classes.iconWrapper}>
+                                            <Image
+                                                width="20"
+                                                height="20"
+                                                src={truckIcon}
+
+                                            />
+                                        </span>
                                         <span>Supply Chain Issues</span>
                                     </Link>
                                 </li>
