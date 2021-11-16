d18a3cc6f6a4b356be0762da447d8e92fc77cb92
satishr <satish@magedelight.com>
2021-11-16 10:14:45 +0530

    compare page changes

--- src/components/CompareListPage/compareListPage.js
+++ src/components/CompareListPage/compareListPage.js
@@ -27,55 +27,58 @@ const CompareListPage = () => {
     const listId = get(compareListData, 'customer.compare_list.uid')
     const items = get(compareListData, 'customer.compare_list.items', [])
     const productCompareFields = [
-        {hideName: true, name: 'Header Block', renderer: itemHeaderBlock},
-        {name: 'Description', path: "description.html", renderer: itemDescriptionBlock},
-        {name: 'Unit Of Measure', path: "uom"},
-        {name: 'SKU', path: "sku"},
-        {name: 'Availability', path: "availability"},
+        { hideName: true, name: 'Header Block', renderer: itemHeaderBlock },
+        { name: 'Description', path: "description.html", renderer: itemDescriptionBlock },
+        { name: 'Unit Of Measure', path: "uom" },
+        { name: 'SKU', path: "sku" },
+        { name: 'Availability', path: "availability" },
     ]
     return (
         <div className={classes.root}>
             <h1>This is Compare List Page</h1>
             {hasItems ?
-                <table><tbody>
-                    {/** Header of the table for remove item buttons */}
-                    <tr>
-                        <th><Button>Print This page</Button></th>
-                        {items.map((item) => {
-                            return (
-                                <th key={item.uid}>
-                                    <RemoveItemFromCompareList listId={listId} itemId={item.product.id} 
-                                        Child={() => <Button>Remove</Button>}
-                                        Loader={() => <div>Loading...</div>}
-                                    />
-                                </th>
-                            )
-                        })}
-                    </tr>
-                    {productCompareFields.map((compareField) => {
-                        // table row of each field in productCompareFields list
-                        return (
-                            <tr key={compareField.path}>
-                                <td>{!compareField.hideName && compareField.name}</td>
+                <div className={tableWrapper}>
+                    <table className={[classes.data, classes.table, classes.compare].join(" ")} >
+                        <tbody>
+                            {/** Header of the table for remove item buttons */}
+                            <tr>
+                                <th><Button>Print This page</Button></th>
                                 {items.map((item) => {
-                                    const product = item.product;
                                     return (
-                                        <td key={item.uid}>
-                                            {!!compareField.renderer
-                                                ? compareField.renderer(product)
-                                                : (<div>{get(product, compareField.path, "")}</div>)
-                                            }
-                                        </td>
+                                        <th key={item.uid}>
+                                            <RemoveItemFromCompareList listId={listId} itemId={item.product.id}
+                                                Child={() => <Button>Remove</Button>}
+                                                Loader={() => <div>Loading...</div>}
+                                            />
+                                        </th>
                                     )
                                 })}
                             </tr>
-                        )
-                    })}
-                </tbody></table>
+                            {productCompareFields.map((compareField) => {
+                                // table row of each field in productCompareFields list
+                                return (
+                                    <tr key={compareField.path}>
+                                        <td>{!compareField.hideName && compareField.name}</td>
+                                        {items.map((item) => {
+                                            const product = item.product;
+                                            return (
+                                                <td key={item.uid}>
+                                                    {!!compareField.renderer
+                                                        ? compareField.renderer(product)
+                                                        : (<div>{get(product, compareField.path, "")}</div>)
+                                                    }
+                                                </td>
+                                            )
+                                        })}
+                                    </tr>
+                                )
+                            })}
+                        </tbody></table>
+                </div>
                 :
                 <div>No Items here To compare</div>
             }
-        </div>
+        </div >
     )
 }
 
@@ -85,7 +88,7 @@ export default CompareListPage
 const itemHeaderBlock = (item) => {
     return (
         <div>
-            <img src={get(item, 'image.url', "")} style={{maxWidth: '200px'}} />
+            <img src={get(item, 'image.url', "")} style={{ maxWidth: '200px' }} />
             <div>{item.name}</div>
             <div>{item.price}</div>
         </div>
