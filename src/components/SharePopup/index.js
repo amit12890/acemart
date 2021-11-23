d26c48d8da93c2d241c273e93c987ce402f47627
satishr <satish@magedelight.com>
2021-11-23 19:28:50 +0530

    share section fixed

--- src/components/SharePopup/index.js
+++ src/components/SharePopup/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Portal } from '@magento/venia-ui/lib/components/Portal';
+import { Link } from 'react-router-dom';
+import Image from '../../venia/components/Image';
+import iconFacebook from '../../assets/facebook.svg';
+import iconTwitter from '../../assets/twitter.svg';
+import iconPinterest from '../../assets/pinterest.svg';
+import iconLinkedIn from '../../assets/linkedIn.svg';
+import iconEmail from '../../assets/email.svg';
+
 
 import Button from '../../venia/components/Button';
 import { useStyle } from '../../venia/classify';
 
-import defaultClasses from '../WishList/wishlistPopup.css';
+import defaultClasses from '../SharePopup/sharePopup.css';
 
 const SharePopup = props => {
     const { closeSharePopup, productId } = props;
@@ -38,6 +46,61 @@ const SharePopup = props => {
                             <div className={classes.heading}>
                                 Share This Item
                             </div>
+                            <div className={classes.shareButtons}>
+                                <div className={classes.shareItemWraper}>
+                                    <div className={classes.shareItem}>
+                                        <a href="#" className={classes.shareLink}>
+                                            <Image
+                                                src={iconFacebook}
+                                                width="25"
+                                            />
+                                            <span>Share this on Facebook</span>
+                                        </a>
+                                    </div>
+
+                                    <div className={classes.shareItem}>
+                                        <a href="#" className={classes.shareLink}>
+                                            <Image
+                                                src={iconTwitter}
+                                                width="25"
+                                            />
+                                            <span>Tweet this on Twitter</span>
+                                        </a>
+                                    </div>
+
+                                    <div className={classes.shareItem}>
+                                        <a href="#" className={classes.shareLink}>
+                                            <Image
+                                                src={iconPinterest}
+                                                width="25"
+                                            />
+                                            <span>Pin this on Pinterest</span>
+                                        </a>
+                                    </div>
+
+                                    <div className={classes.shareItem}>
+                                        <a href="#" className={classes.shareLink}>
+                                            <Image
+                                                src={iconLinkedIn}
+                                                width="25"
+                                            />
+                                            <span>Share it on LinkedIn</span>
+                                        </a>
+                                    </div>
+
+                                    <div className={classes.shareItem}>
+                                        <a href="#" className={classes.shareLink}>
+                                            <Image
+                                                src={iconEmail}
+                                                width="25"
+                                            />
+                                            <span>Email This Information</span>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
