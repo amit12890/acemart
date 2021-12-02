3eb98c1a1c4931f51a994bc8201ba32c187c5355
Dishant Chavda <dishant.chadva@magedelight.com>
2021-12-02 18:37:50 +0530

    completed que & ans filter, expand functionality

--- src/components/ProductQuestions/productQuestionsBlock.js
+++ src/components/ProductQuestions/productQuestionsBlock.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { useQuery } from '@apollo/client';
+import Fuse from "fuse.js";
+import { get, size, range } from 'lodash';
 
-import { get, size } from 'lodash';
-
-// import { useStyle } from '../../classify';
-// import defaultClasses from './productFullDetail.css';
+import Button from '../../venia/components/Button';
+import { useStyle } from '@magento/venia-ui/lib/classify';
+import defaultClasses from './productQuestions.css';
 import AddQuestion from './addQuestion';
 import AddAnswer from "./addAnswer";
 import { getProductQuestions } from './productQuestions.gql';
 
 const ProductQuestionsBlock = ({ productId }) => {
+    const classes = useStyle(defaultClasses);
+
     const {
         data: questionsData,
         loading: questionsDataLoading,
@@ -30,7 +33,7 @@ const ProductQuestionsBlock = ({ productId }) => {
     } else {
         const hasItems = get(questionsData, 'questions.items.length');
         if (hasItems) {
-            content = <QuestionBlock data={questionsData.questions.items} />;
+            content = <QuestionBlock questions={questionsData.questions.items} />;
         } else {
             content = <div>Be the first one to ask question.</div>;
         }
@@ -44,40 +47,105 @@ const ProductQuestionsBlock = ({ productId }) => {
     );
 };
 
-const QuestionBlock = ({ data }) => {
-    return data.map((item, index) => {
-        const ansCount = size(item.answer);
-        return (
-            <div key={index}>
-                <div className="question">
-                    <div>{item.content}</div>
-                    <div>
-                        {ansCount > 1
-                            ? `${ansCount} answers`
-                            : `${ansCount} answer`}
-                    </div>
-                    <div>by {item.nickname}</div>
-                </div>
-                <div className="answer">
-                    <div>
-                        {!!ansCount
-                            ? item.answer.map((ans, ind) => {
-                                  return (
-                                      <div key={ind}>
-                                          <div>{ans.content}</div>
-                                          <div>by {ans.nickname}</div>
-                                      </div>
-                                  );
-                              })
-                            : null}
-                    </div>
-                    <div>
-                        <AddAnswer questionId={item.id} />
-                    </div>
-                </div>
+const QuestionBlock = ({ questions }) => {
+console.log("🚀 ~ file: productQuestionsBlock.js ~ line 51 ~ QuestionBlock ~ questions", questions)
+    const classes = useStyle(defaultClasses);
+    const [expandedQuestions, setExpandedQuestions] = useState(new Set([]))
+    const [searchToken, setSearchToken] = useState("")
+    const fuseSearch = useRef();
+
+    useEffect(() => {
+        // setup fuse search
+        const options = {
+            shouldSort: true,
+            // tokenize: true,
+            findAllMatches: true,
+            threshold: 0.8,
+            location: 0,
+            distance: 32,
+            maxPatternLength: 32,
+            minMatchCharLength: 2,
+            keys: [
+                  "content",
+              ]
+          };
+          fuseSearch.current = new Fuse(questions, options);
+    }, [questions])
+
+    const handleQueExpandToggle = useCallback((queIndex) => {
+        const newSet = new Set(expandedQuestions);
+        if (newSet.has(queIndex)) newSet.delete(queIndex);
+        else newSet.add(queIndex);
+        setExpandedQuestions(newSet)
+    }, [expandedQuestions, setExpandedQuestions])
+
+    const handleExpandAll = useCallback(() => {
+        setExpandedQuestions(new Set(range(0, questions.length)))
+    }, [questions, setExpandedQuestions])
+
+    const handleResetSearch = useCallback(() => {
+        setSearchToken("");
+    }, [])
+
+    const filteredQuestions = useMemo(() => {
+        if (searchToken.length > 2) {
+          return fuseSearch.current.search(searchToken);
+        } else {
+          return questions;
+        }
+      }, [searchToken, questions]);
+
+    return (
+        <div>
+            <div>
+                <label>Search Q/A</label>
+                <input placeholder="Search Phrase" value={searchToken}
+                    onChange={(e) => setSearchToken(e.target.value)} />
+                <Button onClick={handleResetSearch}>Reset</Button>
             </div>
-        );
-    });
+            <div>
+                <label>Sort By</label>
+                <Button onClick={handleExpandAll}>Expand All</Button>
+            </div>
+
+            {filteredQuestions.map((item, index) => {
+                const ansCount = size(item.answer);
+                let queClass = expandedQuestions.has(index)
+                    ? `${classes.question} ${classes.question_open}` : classes.question
+
+                return (
+                    <div key={item.id}>
+                        <div className={queClass} onClick={() => handleQueExpandToggle(index)}>
+                            <div>{item.content}</div>
+                            <div>
+                                {ansCount > 1
+                                    ? `${ansCount} answers`
+                                    : `${ansCount} answer`}
+                            </div>
+                            <div>by {item.nickname}</div>
+                        </div>
+                        <div className="answer">
+                            <div>
+                                {!!ansCount
+                                    ? item.answer.map((ans, ind) => {
+                                        return (
+                                            <div key={ind}>
+                                                <div>{ans.content}</div>
+                                                <div>by {ans.nickname}</div>
+                                            </div>
+                                        );
+                                    })
+                                    : null}
+                            </div>
+                            <div>
+                                <AddAnswer questionId={item.id} />
+                            </div>
+                        </div>
+                    </div>
+                )})
+            }
+        </div>
+    );
 };
 
 export default ProductQuestionsBlock;
