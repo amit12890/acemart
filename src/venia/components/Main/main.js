be04ac5998307ae4d92dc879383d068e2973246d
Dishant Chavda <dishant.chadva@magedelight.com>
2021-12-06 17:34:22 +0530

    new/checkout page layout update

--- src/venia/components/Main/main.js
+++ src/venia/components/Main/main.js
@@ -1,28 +1,36 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 import { bool, shape, string } from 'prop-types';
 import { useScrollLock } from '@magento/peregrine';
 
 import { useStyle } from '@magento/venia-ui/lib/classify';
 import Footer from '../Footer';
 import Header from '../Header';
+import CheckoutHeader from '../CheckoutHeader';
 import defaultClasses from './main.css';
-
 
 
 const Main = props => {
     const { children, isMasked } = props;
+    let { pathname } = useLocation();
     const classes = useStyle(defaultClasses, props.classes);
 
     const rootClass = isMasked ? classes.root_masked : classes.root;
     const pageClass = isMasked ? classes.page_masked : classes.page;
+    // for checkout page show different header and no footer
+    const showCheckoutLayout = pathname.includes("checkout")
 
     useScrollLock(isMasked);
 
     return (
         <main className={rootClass}>
-            <Header />
+            {showCheckoutLayout ?
+                <CheckoutHeader />
+                :
+                <Header />
+            }
             <div className={pageClass}>{children}</div>
-            <Footer />
+            {!showCheckoutLayout && <Footer />}
         </main>
     );
 };
