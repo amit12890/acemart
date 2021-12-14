91a91f875830768f6d2c023fd4eb6e1057007eb2
Vihas <vihas.shah@krishtechnolabs.com>
2021-12-14 12:13:53 +0530

    [ACEMART-377] Review List UI updated - review list block only display if review has data

--- src/@amasty/components/ProductReviews/productReviews.js
+++ src/@amasty/components/ProductReviews/productReviews.js
@@ -7,73 +7,76 @@ import defaultClasses from './productReviews.css';
 import Summary from '../Summary';
 import List from './list';
 import ReviewForm from '../ReviewForm';
+import { size } from 'lodash-es';
 
 const ProductReviews = props => {
-  const { product } = props;
-  const { id, name } = product;
-  const reviewFormRef = useRef(null);
-  const rootRef = useRef(null);
+    const { product } = props;
+    const { id, name } = product;
+    const reviewFormRef = useRef(null);
+    const rootRef = useRef(null);
 
-  const {
-    loading,
-    error,
-    productReviews,
-    goToReviewForm,
-    settings,
-    settingsLoading,
-    pageControl,
-    toolbarValues,
-    handleChangeToolbar,
-    handleStarFilter
-  } = useProductReviews({
-    id,
-    reviewFormRef,
-    rootRef
-  });
+    const {
+        loading,
+        error,
+        productReviews,
+        goToReviewForm,
+        settings,
+        settingsLoading,
+        pageControl,
+        toolbarValues,
+        handleChangeToolbar,
+        handleStarFilter
+    } = useProductReviews({
+        id,
+        reviewFormRef,
+        rootRef
+    });
 
-  if (settingsLoading) {
-    return <LoadingIndicator />;
-  }
+    if (settingsLoading) {
+        return <LoadingIndicator />;
+    }
 
-  if (!productReviews || error) {
-    return null;
-  }
+    if (!productReviews || error) {
+        return null;
+    }
 
-  const classes = mergeClasses(defaultClasses, props.classes);
+    const classes = mergeClasses(defaultClasses, props.classes);
 
-  return (
-    <div ref={rootRef} className={classes.root}>
-      <div className={classes.title}>Customer Reviews</div>
+    return (
+        <div ref={rootRef} className={classes.root}>
+            {size(productReviews.items) !== 0 && (
+                <>
+                    <div className={classes.title}>Customer Reviews</div>
+                    <Summary
+                        {...productReviews}
+                        goToReviewForm={goToReviewForm}
+                        handleStarFilter={handleStarFilter}
+                    />
 
-      <Summary
-        {...productReviews}
-        goToReviewForm={goToReviewForm}
-        handleStarFilter={handleStarFilter}
-      />
-
-      <List
-        loading={loading}
-        {...productReviews}
-        {...settings}
-        pageControl={pageControl}
-        values={toolbarValues}
-        onChange={handleChangeToolbar}
-        handleStarFilter={handleStarFilter}
-      />
-
-      <div ref={reviewFormRef} className={classes.reviewFormWrapper}>
-        <div className={classes.reviewFormContainer}>
-          <ReviewForm {...settings} productName={name} productId={id} />
+                    <List
+                        loading={loading}
+                        {...productReviews}
+                        {...settings}
+                        pageControl={pageControl}
+                        values={toolbarValues}
+                        onChange={handleChangeToolbar}
+                        handleStarFilter={handleStarFilter}
+                    />
+                </>
+            )}
+            <div ref={reviewFormRef} className={classes.reviewFormWrapper}>
+                <div className={classes.reviewFormContainer}>
+                    <ReviewForm {...settings} productName={name} productId={id} />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 ProductReviews.propTypes = {
-  product: shape({
-    id: number.isRequired
-  })
+    product: shape({
+        id: number.isRequired
+    })
 };
 
 export default ProductReviews;
