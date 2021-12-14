8c9aab4e2453344683a4cc69334412b590ba6af1
Vihas <vihas.shah@krishtechnolabs.com>
2021-12-14 18:55:34 +0530

    [ACEMART-255] search api call - auth header empty check condition updated

--- src/data.utils.js
+++ src/data.utils.js
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import Axios from 'axios';
 
 import { useUserContext } from '@magento/peregrine/lib/context/user';
+import { size } from 'lodash-es';
 
 
 export const useApiData = ({ url = "", method = "get", headers = {}, data = {}, isLazy = false, onSuccess = null }) => {
@@ -14,12 +15,17 @@ export const useApiData = ({ url = "", method = "get", headers = {}, data = {}, 
         async (cbUrl = null, data = {}) => {
             setLoading(true);
             try {
+                let requestHeaders = { ...headers }
+                // auth header will be only passed if token has values
+                if (size(token) > 0) {
+                    requestHeaders = {
+                        ...requestHeaders,
+                        authorization: token ? `Bearer ${token}` : ''
+                    }
+                }
                 const response = await Axios({
                     method, url: cbUrl ? cbUrl : url,
-                    headers: {
-                        authorization: token ? `Bearer ${token}` : '',
-                        ...headers,
-                    },
+                    headers: requestHeaders,
                     data,
                 });
                 setResponse(response.data);
