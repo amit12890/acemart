aff6cf38ad5bb34e823294dbd45eff88556ca163
Dishant Chavda <dishant.chadva@magedelight.com>
2022-01-02 15:34:04 +0530

    fix/clear search token on redirect

--- src/venia/components/SearchBar/searchField.js
+++ src/venia/components/SearchBar/searchField.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { func } from 'prop-types';
 import { Search as SearchIcon, X as ClearIcon } from 'react-feather';
-import { useSearchField } from '@magento/peregrine/lib/talons/SearchBar';
-
 import Icon from '@magento/venia-ui/lib/components/Icon';
 import TextInput from '../TextInput';
 import Trigger from '@magento/venia-ui/lib/components/Trigger';
+import { useLocation } from 'react-router-dom';
 
-import defaultClasses from './searchField.css'
+import defaultClasses from './searchField.css';
 import { useStyle } from '../../classify';
+import { useSearchField } from '../../../magento/peregrine/talons/SearchBar/useSearchField';
+import { searchPage } from '../../../url.utils';
 
 const clearIcon = <Icon src={ClearIcon} size={24} />;
 const searchIcon = <Icon src={SearchIcon} size={24} />;
 
 const SearchField = props => {
-    const classes = useStyle(defaultClasses)
+    const classes = useStyle(defaultClasses);
     const { isSearchOpen, onChange, onFocus } = props;
     const { inputRef, resetForm, value } = useSearchField({ isSearchOpen });
+
+    const location = useLocation();
+    const { pathname } = location;
+
+    useEffect(() => {
+        if (pathname !== searchPage()) {
+            resetForm();
+        }
+    }, [pathname]);
 
     const resetButton = value ? (
         <Trigger action={resetForm}>{clearIcon}</Trigger>
@@ -24,13 +34,13 @@ const SearchField = props => {
 
     return (
         <TextInput
-            after={value ? resetButton : searchIcon}    
+            after={value ? resetButton : searchIcon}
             field="search_query"
             onFocus={onFocus}
             onValueChange={onChange}
             forwardedRef={inputRef}
             classes={{
-                input:classes.searchTextInput
+                input: classes.searchTextInput
             }}
             placeholder="Search Ace Mart"
         />
