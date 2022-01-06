3bd128d28f10ceee499c760856924f112c355066
Dishant Chavda <dishant.chadva@magedelight.com>
2022-01-06 13:26:42 +0530

    fix/api utils updated

--- src/data.utils.js
+++ src/data.utils.js
@@ -4,8 +4,15 @@ import Axios from 'axios';
 import { useUserContext } from '@magento/peregrine/lib/context/user';
 import { size } from 'lodash-es';
 
-
-export const useApiData = ({ url = "", method = "get", headers = {}, data = {}, isLazy = false, onSuccess = null }) => {
+export const useApiData = ({
+    url = '',
+    method = 'get',
+    headers = {},
+    data = {},
+    isLazy = false,
+    useAuth = true,
+    onSuccess = null
+}) => {
     const [{ token }, _] = useUserContext();
     const [loading, setLoading] = useState(!isLazy);
     const [response, setResponse] = useState({});
@@ -15,16 +22,18 @@ export const useApiData = ({ url = "", method = "get", headers = {}, data = {}, 
         async (cbUrl = null, data = {}) => {
             setLoading(true);
             try {
-                let requestHeaders = { ...headers }
+                let requestHeaders = { ...headers };
                 // auth header will be only passed if token has values
-                if (size(token) > 0) {
-                    requestHeaders = requestHeaders ? 
-                        requestHeaders : { authorization : token ? `Bearer ${token}` : '' }
+                if (size(token) > 0 && useAuth) {
+                    requestHeaders['authorization'] = token
+                        ? `Bearer ${token}`
+                        : '';
                 }
                 const response = await Axios({
-                    method, url: cbUrl ? cbUrl : url,
+                    method,
+                    url: cbUrl ? cbUrl : url,
                     headers: requestHeaders,
-                    data,
+                    data
                 });
                 setResponse(response.data);
                 setLoading(false);
@@ -32,47 +41,47 @@ export const useApiData = ({ url = "", method = "get", headers = {}, data = {}, 
             } catch (error) {
                 setError(true);
                 setLoading(false);
-                console.log("file: data.utils.js ~ line 27 ~ error", error)
+                console.log('file: data.utils.js ~ line 27 ~ error', error);
             }
-        }, [url, onSuccess]
-    )
+        },
+        [url, onSuccess, token]
+    );
 
     useEffect(() => {
         // if isLazy call api manually
         // else api will be called as soon as component renders
-        if (!isLazy)
-            callApi(data);
-    }, [])
+        if (!isLazy) callApi(data);
+    }, []);
 
     return { callApi, loading, response, error };
-}
+};
 
 /**
  * extract message from object and check for auth error
  * if there is auth error then it will return message as well as clear customer data if customersis logged in
- * 
+ *
  * if error is already parsed then it will return same error as got in param
- * 
+ *
  * @param {Object} error => default graphql error
  * @return { code , message } => for auth code will be 401 else it will 400. code are custom added
  */
-export const parseGraphQLError = (error) => {
-    if (has(error, "code")) {
-        return error
+export const parseGraphQLError = error => {
+    if (has(error, 'code')) {
+        return error;
     }
-    let gqlCategory = get(error, "graphQLErrors[0].extensions.category", "")
-    let errorMessage = get(error, "graphQLErrors[0].message", "")
-    if (gqlCategory === "graphql-authorization") {
+    let gqlCategory = get(error, 'graphQLErrors[0].extensions.category', '');
+    let errorMessage = get(error, 'graphQLErrors[0].message', '');
+    if (gqlCategory === 'graphql-authorization') {
         store.dispatch({
             type: 'LOGOUT'
-        })
+        });
         return {
             code: 401,
             message: errorMessage
-        }
+        };
     }
     return {
         code: 400,
         message: errorMessage
-    }
-}
+    };
+};
