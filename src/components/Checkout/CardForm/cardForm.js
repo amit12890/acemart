3d64ac2cf02002c1430a787ad0914b6999b4cceb
Vihas <vihas.shah@krishtechnolabs.com>
2022-01-12 02:22:34 +0530

    [ACEMART-288] Firstdata Payment Gatway integration - custom card component created and UI completed - set payment methd graphql integration completed

---
+++ src/components/Checkout/CardForm/cardForm.js
@@ -0,0 +1,158 @@
+import React, { useEffect, useReducer } from 'react'
+
+import { PaymentInputsContainer } from 'react-payment-inputs'
+import images from 'react-payment-inputs/images'
+
+import LoadingButton from '../../LoadingButton'
+import Checkbox from '../../Checkbox'
+
+import classify from '@magento/venia-ui/lib/classify'
+import { compact, get, has, size, values } from 'lodash'
+
+import defaultClasses from './cardForm.css'
+
+
+const CardInput = ({ error, classes, cardProps, ...restProps }) => {
+    const isError = size(error) > 0
+    return (
+        <div className={[classes.block, classes.inputMargin].join(" ")}>
+            <input className={[classes.input, isError ? classes.errorBorder : ''].join(" ")}
+                {...cardProps} {...restProps} />
+            {isError && (
+                <div className={classes.mageError}>
+                    {error}
+                </div>
+            )}
+        </div>
+    )
+}
+
+
+class CardForm extends React.Component {
+
+    state = {
+        cardNumber: '',
+        cardType: '',
+        cvv: '',
+        expiry: '',
+        saveCard: false,
+        cardName: ''
+    }
+
+    submit = (e, cardType) => {
+        e.preventDefault()
+        const expArr = this.state.expiry.split(" / ")
+        console.log({
+            cc_number: this.state.cardNumber,
+            cc_type: "MC",
+            cc_cid: this.state.cvv,
+            expiration: expArr[0].trim(),
+            expiration_yr: expArr[1].trim(),
+            save_card: this.state.saveCard,
+            transarmor_id: "new"
+        })
+        this.props.onSave({
+            cc_number: this.state.cardNumber.replace(/ /g, ''),
+            cc_type: "MC",
+            cc_cid: this.state.cvv,
+            expiration: expArr[0].trim(),
+            expiration_yr: expArr[1].trim(),
+            save_card: this.state.saveCard,
+            transarmor_id: "new"
+        })
+    }
+
+    handleInput = (identifier, e) => {
+        e.preventDefault()
+        this.setState({ [identifier]: e.target.value })
+    }
+
+    render = () => {
+        const { classes } = this.props
+        return (
+            <PaymentInputsContainer>
+                {({ meta, getCardImageProps, getCardNumberProps, getExpiryDateProps, getCVCProps }) => {
+                    const { erroredInputs, touchedInputs } = meta;
+                    return (
+                        <div className={classes.cardBlock}>
+                            <div className={classes.cardContent}>
+                                <fieldset className={classes.fieldset}>
+                                    <div className={classes.cardField}>
+                                        <div className={classes.block}>
+                                            <input
+                                                name="cardName"
+                                                value={this.state.cardName}
+                                                placeholder="Card Name"
+                                                onChange={(e) => this.handleInput("cardName", e)}
+                                                className={classes.input} />
+                                        </div>
+                                    </div>
+                                    <div className={classes.cardField}>
+                                        <div className={classes.cardIcon}>
+                                            <svg {...getCardImageProps({ images })} width={'3rem'} height={'2.5rem'} />
+                                        </div>
+                                        <CardInput
+                                            cardProps={{ ...getCardNumberProps({ onChange: (e) => this.handleInput("cardNumber", e) }) }}
+                                            value={this.state.cardNumber}
+                                            error={touchedInputs.cardNumber && erroredInputs.cardNumber}
+                                            classes={{ ...classes, inputMargin: classes.leftMargin }} />
+                                    </div>
+                                    <div className={classes.cardField}>
+                                        <CardInput
+                                            cardProps={{ ...getExpiryDateProps({ onChange: e => this.handleInput("expiry", e) }) }}
+                                            value={this.state.expiry}
+
+                                            error={touchedInputs.expiryDate && erroredInputs.expiryDate}
+                                            classes={classes} />
+                                        <CardInput
+                                            cardProps={{ ...getCVCProps({ onChange: e => this.handleInput("cvv", e) }) }}
+                                            value={this.state.cvv}
+                                            error={touchedInputs.cvc && erroredInputs.cvc}
+                                            classes={{ ...classes, inputMargin: classes.leftMargin }} />
+                                    </div>
+                                    <div className={classes.checkboxWrapper}>
+                                        <Checkbox
+                                            checked={this.state.saveCard}
+                                            onChange={() => this.setState({ saveCard: !this.state.saveCard })}
+                                            label="Save Card" />
+                                    </div>
+                                </fieldset>
+                            </div>
+                            <div className={classes.actionToolbar}>
+                                <div className={classes.primary}>
+                                    {(false) ? (
+                                        <LoadingButton classes={{ wrapper: classes.loadingButton }} />
+                                    ) : (
+                                        <button
+                                            type="submit"
+                                            className={classes.action}
+                                            onClick={(e) => {
+                                                const hasErrors = size(compact(values(erroredInputs))) > 0
+                                                if (!hasErrors) {
+                                                    this.submit(e, get(meta, "cardType.type", {}))
+                                                }
+                                            }}
+                                            disabled={false}>
+                                            <span>Save</span>
+                                        </button>
+                                    )}
+                                </div>
+                                <div className={classes.primary}>
+                                    <button
+                                        className={[classes.action, classes.secondaryButton].join(" ")}
+                                        onClick={this.props.toggleCardForm}
+                                        disabled={false}>
+                                        <span>Cancel</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div >
+
+                    )
+                }}
+            </PaymentInputsContainer>
+        )
+    }
+}
+
+export default classify(defaultClasses)(CardForm)
