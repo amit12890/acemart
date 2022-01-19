c3953e5ffd5bacb5c1091e287d33358dec580df0
Dishant Chavda <dishant.chadva@magedelight.com>
2022-01-19 18:33:57 +0530

    update/no search content added

--- src/venia/components/SearchPage/searchPage.js
+++ src/venia/components/SearchPage/searchPage.js
@@ -1,6 +1,7 @@
 import React, { Fragment, Suspense, useMemo } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { shape, string } from 'prop-types';
+import { AlertTriangle } from 'react-feather';
 
 import { useSearchPage } from '../../../magento/peregrine/talons/SearchPage/useSearchPage';
 
@@ -97,6 +98,12 @@ const SearchPage = props => {
             return (
                 <div className={classes.noResultWrapper}>
 
+                    <div className={classes.noResultText}>
+                        <AlertTriangle />
+                        <span>
+                            Sorry no results found for "{searchTerm}"
+                        </span>
+                    </div>
                     {didYouMean ? (
                         <div className={classes.didYouMean}>
                             Did you mean: <Link to={`${searchPage()}?q=${didYouMean.query}`}><RichText content={didYouMean.highlighted} /></Link>?
