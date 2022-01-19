87ee99d77c95d3c05c41ca859007bcd5cce91a71
satishr <satish@magedelight.com>
2022-01-19 19:42:33 +0530

    footer cms block update

--- src/venia/components/Footer/footer.js
+++ src/venia/components/Footer/footer.js
@@ -152,7 +152,7 @@ const Footer = props => {
                                         classes={{ root: classes.footerContent }}
                                         html={footerCol1} />
                                 )}
-                                <h3 className={classes.groupTitle}>Customer Center</h3>
+                                {/* <h3 className={classes.groupTitle}>Customer Center</h3>
                                 <ul className={classes.groupLinks}>
                                     <li className={classes.link}>
                                         <Link
@@ -191,15 +191,15 @@ const Footer = props => {
                                             Shipping Policies
                                         </Link>
                                     </li>
-                                </ul>
+                                </ul> */}
                             </div>
 
                             <div className={classes.footerBox}>
-                                {/* {size(footerCol2) > 0 && (
+                                {size(footerCol2) > 0 && (
                                     <RichContent html={footerCol2} />
-                                )} */}
+                                )}
 
-                                <h3 className={classes.groupTitle}>Company Info</h3>
+                                {/* <h3 className={classes.groupTitle}>Company Info</h3>
                                 <ul className={classes.groupLinks}>
                                     <li className={classes.link}>
                                         <Link
@@ -229,14 +229,14 @@ const Footer = props => {
                                             FAQs
                                         </Link>
                                     </li>
-                                </ul>
+                                </ul> */}
                             </div>
 
                             <div className={classes.footerBox}>
-                                {/* {size(footerCol3) > 0 && (
+                                {size(footerCol3) > 0 && (
                                     <RichContent html={footerCol3} />
-                                )} */}
-                                <h3 className={classes.groupTitle}>Tools & Services</h3>
+                                )}
+                                {/* <h3 className={classes.groupTitle}>Tools & Services</h3>
                                 <ul className={classes.groupLinks}>
                                     <li className={classes.link}>
                                         <Link
@@ -266,14 +266,14 @@ const Footer = props => {
                                             Commercial Kitchen Design
                                         </Link>
                                     </li>
-                                </ul>
+                                </ul> */}
                             </div>
 
                             <div className={classes.footerBox}>
-                                {/* {size(footerCol4) > 0 && (
+                                {size(footerCol4) > 0 && (
                                     <RichContent html={footerCol4} />
-                                )} */}
-                                <h3 className={classes.groupTitle}>Contact Us</h3>
+                                )}
+                                {/* <h3 className={classes.groupTitle}>Contact Us</h3>
                                 <div className={classes.storeAddress}>
                                     <p>2653 Austin Hwy</p>
                                     <p>San Antonio, TX 78218</p>
@@ -327,7 +327,7 @@ const Footer = props => {
                                             <span>Linkedin</span>
                                         </a>
                                     </li>
-                                </ul>
+                                </ul> */}
                             </div>
 
 
