5728b1bb43dabed55b8eb03a36652b74e87ddbbe
Dishant Chavda <dishant.chadva@magedelight.com>
2022-01-20 14:52:07 +0530

    update/recaptcha key updated

--- src/url.utils.js
+++ src/url.utils.js
@@ -3,7 +3,7 @@ export const HOST_URL = "https://qa-acemart-backend.magedelight.magentoprojects.
 export const FRONT_END_HOST_URL = "https://qa-acemart.magedelight.magentoprojects.net";
 export const SITE_ID = 'qficp5';
 export const GOOGLE_MAP_API_KEY = 'AIzaSyC2FlOz5QUyReshHY4iG6SvWkW0rDF3scY';
-export const GOOGLE_RECAPTCHA = "captcha_site_key"
+export const GOOGLE_RECAPTCHA = "6Len7CMeAAAAANQH3IKiYWqcDeEJ4FNO-tHXEz3I"
 
 /** Handle all url routes */
 
