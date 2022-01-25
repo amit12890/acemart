516a306dbf38907cfe949d10ec18b4d36172c2c4
Dishant Chavda <dishant.chadva@magedelight.com>
2022-01-25 09:50:43 +0530

    update/google analytics integrated

--- src/venia/components/App/container.js
+++ src/venia/components/App/container.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useErrorContext } from '@magento/peregrine/lib/context/unhandledErrors';
+import ReactGA from 'react-ga';
 
 import App from './app';
 import { useErrorBoundary } from '@magento/venia-ui/lib/components/App/useErrorBoundary';
+import { GOOGLE_ANALYTICS_ID } from '../../../url.utils';
 
 const AppContainer = () => {
     const ErrorBoundary = useErrorBoundary(App);
     const [unhandledErrors, errorApi] = useErrorContext();
+
+    useEffect(() => {
+        ReactGA.initialize(GOOGLE_ANALYTICS_ID);
+    }, [])
 
     return <ErrorBoundary unhandledErrors={unhandledErrors} {...errorApi} />;
 };
