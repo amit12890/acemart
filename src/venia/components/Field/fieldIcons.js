e0a5d6adedb801533280a9ffdc83db7865e01356
Vihas <vihas.shah@krishtechnolabs.com>
2022-01-29 11:20:36 +0530

    search field class update

--- src/venia/components/Field/fieldIcons.js
+++ src/venia/components/Field/fieldIcons.js
@@ -3,11 +3,13 @@ import { shape, string } from 'prop-types';
 
 import { useStyle } from '../../classify';
 import defaultClasses from './fieldIcons.css';
+import { size } from 'lodash';
 
 const FieldIcons = props => {
     const { after, before, children } = props;
 
     const classes = useStyle(defaultClasses, props.classes);
+    console.log("🚀 ~ file: .js ~ linefieldIcons 11 ~ classes", classes.after)
 
     const style = {
         '--iconsBefore': before ? 1 : 0,
@@ -18,7 +20,7 @@ const FieldIcons = props => {
         <span className={classes.root} style={style}>
             <span className={classes.input}>{children}</span>
             <span className={classes.beforeClass}>{before}</span>
-            <span className={classes.afterClass}>{after}</span>
+            <span className={[classes.afterClass, size(classes.after) > 0 && classes.after].join(" ")}>{after}</span>
         </span>
     );
 };
